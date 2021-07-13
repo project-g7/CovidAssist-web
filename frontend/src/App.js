05eaf38464ae3a99ece8875d038891667d4ea724
nuvin0214 <nuvinsamadhi1996yahoo@gmail.com>
2021-07-13 20:45:12 +0530

    completed login and registration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
-import './App.css';
-
+import "./App.css";
+import Login from "./components/Login";
+import Welcome from "./components/Welcome";
+import ForgotPassword from "./components/ForgotPassword";
+import Register from "./components/Register";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 function App() {
   return (
-    <div>
-      <p></p>
-      {/* set branch */}
-      {/*CovidAssist*/}
-      {/*nuwan*/}
-{/* nuvin*/}
-    </div>
-    
+    <Router>
+      <div>
+        <Route exact path="/ForgotPassword" component={ForgotPassword}></Route>
+        <Route exact path="/" component={Login}></Route>
+        <Route exact path="/Register" component={Register}></Route>
+        <Route exact path="/Welcome" component={Welcome}></Route>
+      </div>
+    </Router>
   );
 }
 
